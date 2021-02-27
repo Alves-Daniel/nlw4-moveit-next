35a7d7cec32f4d551b5cbf83df6a945ce706c1e9
Alves-Daniel <alves.odaniel7@gmail.com>
2021-02-27 14:06:12 -0400

    atualização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from 'next/head'
-
-import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -21,7 +23,9 @@ export default function Home() {
           <CompletedChallenges/>
           <Countdown/>
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
    </div>
   )
